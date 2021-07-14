2b27481e95e6e2f9c9427f713270b3ea5d0825b2
nicholashanson <nicholashanson27@outlook.com>
2021-07-14 14:49:30 +0800

    Update LED_button_control.c

--- LED_button_control.c
+++ LED_button_control.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-  DDRC = 0x01;
-  PORTC = 0x02;
-  while (1) 
-  {
-    if (PINC & 0x02) 
-    {
-      PORTC |= 0x01;
-		}
+	DDRC = 0x01;
+	PORTC = 0x02;
+  	while (1) 
+  	{
+    		if (PINC & 0x02) 
+    		{
+      			PORTC |= 0x01;
+    		}
 		else 
 		{
-		  PORTC &= 0xFE;
+		  	PORTC &= 0xFE;
 		}
-  }
+  	}
 }
