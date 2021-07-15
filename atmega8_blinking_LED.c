a6af0b79a9eb17a0068b35f55ace5a3070acd8e6
nicholashanson <nicholashanson27@outlook.com>
2021-07-15 13:43:46 +0800

    Create atmega8_blinking_LED.c

---
+++ atmega8_blinking_LED.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED PD4
+
+int main()
+{
+	DDRD |= (1 << LED);       
+	TCCR1B = (1 << CS10) | (1 << CS12);
+	OCR1A = 1562;
+	TCNT1 = 0;
+	while(1)
+	{
+		while ((TIFR & (1 << OCF1A)) == 0);
+		PORTD ^= (1 << LED);
+		TCNT1 = 0;
+		TIFR |= (1 << OCF1A) ; 
+	}
+}
